7de79e3f32fccf4a378353b92f61db7c9ac69995
veronicagg <veronicagg@users.noreply.github.com>
2016-12-19 14:47:50 -0800

    Updating test script to run semantic and model validation, but not fail due to errors

--- test/test.js
+++ test/test.js
@@ -6,6 +6,7 @@ _ = require('lodash'),
 z = require('z-schema'),
 request = require('request'),
 util = require('util');
+spec = require('swagger-tools').specs.v2;
 
 var extensionSwaggerSchemaUrl = "https://raw.githubusercontent.com/Azure/autorest/master/schema/swagger-extensions.json";
 var swaggerSchemaUrl = "http://json.schemastore.org/swagger-2.0";
@@ -17,8 +18,38 @@ var extensionSwaggerSchema;
 var schema4;
 var exampleSchema;
 
+function swaggerName(value){
+  return value.includes("azure-rest-api-specs/arm-storage/2016-01-01/swagger/storage.json");
+}
 var globPath = path.join(__dirname, '../', '/**/swagger/*.json');
 var swaggers = _(glob.sync(globPath));
+// var swaggers = _(glob.sync(globPath)).filter(swaggerName);  // uncomment this statement to validate swagger files returned from the filter.
+
+var childProcess = require('child_process');
+
+function runScript(scriptPath, args, callback) {
+
+    // keep track of whether callback has been invoked to prevent multiple invocations
+    var invoked = false;
+
+    var process = childProcess.fork(scriptPath, args);
+
+    // listen for errors as they may prevent the exit event from firing
+    process.on('error', function (err) {
+        if (invoked) return;
+        invoked = true;
+        callback(err);
+    });
+
+    // execute the callback once the process has finished running
+    process.on('exit', function (code) {
+        if (invoked) return;
+        invoked = true;
+        var err = code === 0 ? null : new Error('exit code ' + code);
+        callback(err);
+    });
+
+}
 
 // Remove byte order marker. This catches EF BB BF (the UTF-8 BOM)
 // because the buffer-to-string conversion in `fs.readFile()`
@@ -33,7 +64,35 @@ function stripBOM(content) {
   return content;
 }
 
-describe('Azure Swagger Schema Validation', function() {
+function specValidator(parsedSpecInJson, callback) {
+  spec.validate(parsedSpecInJson, function (err, result) {
+    if (err) {
+      callback(err);
+    }
+    if (result && result.errors.length > 0) {
+      console.log('');
+      console.log('Errors');
+      console.log('------');
+
+      result.errors.forEach(function (err) {
+          console.log(err.code + ' : ' + '#/' + err.path.join('/') + ' : ' + err.message);
+      });
+      callback(new Error(util.inspect(result.errors, {depth: null})));
+    } else {
+      callback();
+    }
+  });
+}
+
+function parseJSON(swaggerSpecPath, callback) {
+  fs.readFile(swaggerSpecPath, 'utf8', function(err, data) {
+    if(err) { return callback(err); }
+    return callback(null, JSON.parse(stripBOM(data)));
+  });
+}
+
+describe('Azure Open API spec validation for', function() {
+  var parsedData = {};
   before(function(done) {
     request({url: extensionSwaggerSchemaUrl, json:true}, function (error, response, extensionSwaggerSchemaBody) {        
       request({ url: swaggerSchemaAltUrl, json: true }, function (error, response, swaggerSchemaBody) {
@@ -48,11 +107,11 @@ describe('Azure Swagger Schema Validation', function() {
   });
 
   _(swaggers).each(function(swagger){
-    it(swagger + ' should be valid Swagger', function(done){
-      fs.readFile(swagger, 'utf8', function(err, data) {
+    it(swagger + ' should be valid Swagger', function(done) {
+      parseJSON(swagger, function(err, data) {
         if(err) { done(err); }
         
-        var parsedData = JSON.parse(stripBOM(data));
+        parsedData = data;
         if (parsedData.documents && util.isArray(parsedData.documents)) {
           console.log(util.format('Skipping the test for \'%s\' document as it seems to be a composite swagger doc.', swagger));
           done();
@@ -62,12 +121,44 @@ describe('Azure Swagger Schema Validation', function() {
         });
         validator.setRemoteReference(swaggerSchemaUrl, swaggerSchema);
         validator.setRemoteReference(exampleSchemaUrl, exampleSchema);
-        var valid = validator.validate(JSON.parse(stripBOM(data)), extensionSwaggerSchema);
+        var valid = validator.validate(data, extensionSwaggerSchema);
         if (!valid) {
             var error = validator.getLastErrors();
             throw new Error("Schema validation failed: " + JSON.stringify(error, null, "\t"));
         }
         assert(valid === true);
+        done();
+      });
+    });
+    it(swagger + ' should be a valid open api spec using swagger-tools semantic validation\n', function(done) {
+      if (parsedData.documents && util.isArray(parsedData.documents)) {
+        console.log(util.format('Skipping the test for \'%s\' document as it seems to be a composite swagger doc.', swagger));
+        done();
+      }
+      //merge x-ms-paths into paths if they exist
+      if (parsedData['x-ms-paths'] && parsedData['x-ms-paths'] instanceof Object && 
+        Object.keys(parsedData['x-ms-paths']).length > 0) {
+        var paths = parsedData.paths;
+        for (var property in parsedData['x-ms-paths']) {
+          paths[property] = parsedData['x-ms-paths'][property];
+        }
+        parsedData.paths = paths;
+      }
+      specValidator(parsedData, function (err) {
+       // ignore errors for now
+        // if (err) {
+        //   done(err);
+        // } else {
+        //   done();
+        // }
+      done();
+      });
+    });
+    it(swagger + ' should validate against model definition\n', function(done) {
+      runScript('./node_modules/openapi-validator-tools/validate.js',["example", swagger], function (err) {
+        //ignore errors for now
+        //if (err) throw err;
+        console.log('finished running model validator');
         done();
       });
     });
